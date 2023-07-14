1335ea697012a4d308ec3cb0ba27ab93c605a432
Ngô Văn Trung <Trung2601.it@gmail.com>
2023-07-14 17:21:13 +0700

    [update] add new feature :sparkles:

--- app.js
+++ app.js
@@ -3,10 +3,19 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoClient = require('mongoose');
 const logger = require('morgan');
-const { error } = require('console');
-
+// const { error } = require('console');
+const passport = require('passport');
+const session = require('express-session');
+const crypto = require('crypto');
+const cors = require('cors');
+const corsOptions = {
+    origin: ['http://localhost:3000', 'http://127.0.0.1:3000', 'http://localhost:5173', 'http://127.0.0.1:5173'],
+    credentials: true,            //access-control-allow-credentials:true
+    optionSuccessStatus: 200
+}
 // import router
 const productRouter = require('./routes/productRoute');
+const customerRouter = require('./routes/customerRoute');
 
 // setup connect mongodb by mongoose
 // Client
@@ -34,27 +43,34 @@ mongoClient.connect('mongodb://127.0.0.1:27017/morning-basket', { useNewUrlParse
 
 // Create app object
 const app = express();
+const secret = crypto.randomBytes(64).toString('hex');
 
 // Use middleware
+app.use(cors(corsOptions));
 app.use(logger('dev'));
 app.use(bodyParser.json());
+app.use(require('express-session')({ secret: secret, resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Router
 
-app.use('/products', productRouter)
+app.use('/products', productRouter);
+app.use('/customers', customerRouter);
 
 app.use('/', (req, res, next) => {
     return res.status(200).json({
         message: 'Server is OK'
     })
-})
+});
 
 // Error handle function
 
 app.use((err, req, res, next) => {
     return res.status(err.status || 500).json({
         error: {
-            message: err.message
+            message: err.message,
+            note: "end handle err"
         }
     })
 });
@@ -62,3 +78,4 @@ app.use((err, req, res, next) => {
 app.listen(3000, () => {
     console.log('http://localhost:3000');
 })
+
