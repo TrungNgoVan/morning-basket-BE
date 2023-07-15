de820fc18531336df4356fa73d7fe9a3aebf91a4
Ngô Văn Trung <Trung2601.it@gmail.com>
2023-07-15 08:39:20 +0700

    [update] fix handle error function :hammer:

--- app.js
+++ app.js
@@ -69,8 +69,7 @@ app.use('/', (req, res, next) => {
 app.use((err, req, res, next) => {
     return res.status(err.status || 500).json({
         error: {
-            message: err.message,
-            note: "end handle err"
+            message: err.message
         }
     })
 });
