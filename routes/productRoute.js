488020b513e01fb515f8c5a1e5a9777e0dcff1f9
Ngô Văn Trung <Trung2601.it@gmail.com>
2023-07-15 08:38:43 +0700

    [update] use validate & authenticate for router :sparkles:

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -2,15 +2,30 @@
 
 const express = require('express');
 const router = express.Router();
-
+const { authenticateToken } = require('../middlewares/authenticateToken');
 const productController = require('../controllers/productController');
+const { schemas, validateParam, validateBody } = require('../helpers/routeHelper')
+
 
 router.route('/:productID')
-    .patch(productController.updateProduct)
-    .delete(productController.deleteProduct)
+    .get(
+        validateParam(schemas.idSchema, 'productID'),
+        authenticateToken,
+        productController.getProductByID
+    )
+    .patch(
+        validateParam(schemas.idSchema, 'productID'),
+        productController.updateProduct
+    )
+    .delete(
+        validateParam(schemas.idSchema, 'productID'),
+        productController.deleteProduct
+    )
 
 router.route('/')
     .get(productController.getProducts)
-    .post(productController.createProduct)
+    .post(
+        productController.createProduct
+    )
 
 module.exports = router;
