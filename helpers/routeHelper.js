0bf20090ad90f04662236d58da2b51e3354d1d8f
Ngô Văn Trung <Trung2601.it@gmail.com>
2023-07-16 12:14:14 +0700

    [fix & add] id & body schema of customer, product & order :sparkles:

--- helpers/routeHelper.js
+++ helpers/routeHelper.js
@@ -3,6 +3,7 @@
 const Joi = require('@hapi/joi')
 
 const schemas = {
+    //! Authenticate Schema
     authSignUpSchema: Joi.object()
         .keys({
             name: Joi.string().min(2).required(),
@@ -23,19 +24,24 @@ const schemas = {
         .xor('email', 'phoneNumber')
         .with('email', 'password')
         .with('phoneNumber', 'password'),
+    //! ID Schema 
+    // id mongodb
+    idSchema: Joi.object()
+        .keys({
+            param: Joi.string()
+                .regex(/^[0-9a-fA-F]{24}$/)
+                .required(),
+        }),
+    // id number (1,2,3,...)
+    idNumberSchema: Joi.object()
+        .keys({
+            param: Joi.string()
+                .regex(/^[0-9]{1,3}$/)
+                .required(),
+        }),
 
-    idSchema: Joi.object().keys({
-        param: Joi.string()
-            .regex(/^[0-9a-fA-F]{24}$/)
-            .required(),
-    }),
-
-    productIdSchema: Joi.object().keys({
-        param: Joi.string()
-            .regex(/^[0-9]{1,3}$/)
-            .required(),
-    }),
-
+    //! Body Schema
+    // Customer
     customerSchema: Joi.object()
         .keys({
             name: Joi.string().min(2).required(),
@@ -45,13 +51,78 @@ const schemas = {
         })
         .xor('email', 'phoneNumber')
         .with('email', 'password')
-        .with('phoneNumber', 'password'),
+        .with('phoneNumber', 'password')
+        .options({ stripUnknown: true }),
 
-    customerOptionalSchema: Joi.object().keys({
-        firstName: Joi.string().min(2),
-        lastName: Joi.string().min(2),
-        email: Joi.string().email(),
+    customerOptionalSchema: Joi.object()
+        .keys({
+            name: Joi.string().min(2),
+            email: Joi.string().email(),
+            phoneNumber: Joi.string().pattern(/^[0-9]{10}$/),
+            password: Joi.string().min(6),
+        }),
+    // Product
+    productSchema: Joi.object()
+        .keys({
+            barcode: Joi.string().required(),
+            name: Joi.string().required(),
+            description: Joi.string().required(),
+            price: Joi.number().positive().required(),
+            quantity: Joi.number().positive().required(),
+            photo: Joi.string().allow(null, ''),
+            tags: Joi.array().items(Joi.string()),
+        }),
+    productOptionalSchema: Joi.object().keys({
+        barcode: Joi.string(),
+        name: Joi.string(),
+        description: Joi.string(),
+        price: Joi.number().positive(),
+        quantity: Joi.number().positive(),
+        photo: Joi.string().allow(null, ''),
+        tags: Joi.array().items(Joi.string()),
     }),
+    // Order
+    orderSchema: Joi.object()
+        .keys({
+            customerId: Joi.number().required(),
+            items: Joi.array().items(
+                Joi.object({
+                    itemId: Joi.number().required(),
+                    name: Joi.string().required(),
+                    quantity: Joi.number().required(),
+                    price: Joi.number().required(),
+                })
+            ).required(),
+            totalPrice: Joi.number().required(),
+            shippingAddress: Joi.string().required(),
+            billingAddress: Joi.string().required(),
+            status: Joi.string().valid('pending', 'processing', 'shipped', 'delivered').required(),
+            orderedAt: Joi.date(),
+            createdAt: Joi.date(),
+            updatedAt: Joi.date(),
+        })
+        .options({ stripUnknown: true }),
+    orderOptionalSchema: Joi.object()
+        .keys({
+            customerId: Joi.number(),
+            items: Joi.array().items(
+                Joi.object({
+                    itemId: Joi.number(),
+                    name: Joi.string(),
+                    quantity: Joi.number(),
+                    price: Joi.number(),
+                })
+            ),
+            totalPrice: Joi.number(),
+            shippingAddress: Joi.string(),
+            billingAddress: Joi.string(),
+            status: Joi.string().valid('pending', 'processing', 'shipped', 'delivered'),
+            orderedAt: Joi.date(),
+            createdAt: Joi.date(),
+            updatedAt: Joi.date(),
+        })
+        .options({ stripUnknown: true }),
+
 }
 
 const validateBody = (schema) => {
