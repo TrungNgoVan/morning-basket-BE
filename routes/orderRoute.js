809250e29e8af9783ac75a9da78a7ddbb6fa3233
Ngô Văn Trung <Trung2601.it@gmail.com>
2023-07-16 14:24:38 +0700

    [create] order router :sparkles:

---
+++ routes/orderRoute.js
@@ -0,0 +1,40 @@
+'use strict'
+
+const express = require('express')
+const router = express.Router()
+
+const OrderController = require('../controllers/orderController')
+
+const {
+    schemas,
+    validateParam,
+    validateBody,
+} = require('../helpers/routeHelper')
+
+router
+    .route('/')
+    .get(
+        OrderController.getOrders
+    )
+    .post(
+        validateBody(schemas.orderSchema),
+        OrderController.createOrder
+    )
+
+router
+    .route('/:orderID')
+    .get(
+        validateParam(schemas.idNumberSchema, 'orderID'),
+        OrderController.getOrderByID
+    )
+    .patch(
+        validateParam(schemas.idNumberSchema, 'orderID'),
+        validateBody(schemas.orderOptionalSchema),
+        OrderController.updateOrder
+    )
+    .delete(
+        validateParam(schemas.idNumberSchema, 'orderID'),
+        OrderController.deleteOrder
+    )
+
+module.exports = router;
