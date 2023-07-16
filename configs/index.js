b60ae5513f7ab8f4bd00b95ccf700659cd428f25
Ngô Văn Trung <Trung2601.it@gmail.com>
2023-07-16 12:10:36 +0700

    [resolve] set jwt_secret fixed :hammer:

--- configs/index.js
+++ configs/index.js
@@ -1,6 +1,7 @@
 'use strict'
-const crypto = require('crypto')
-const JWT_SECRET = crypto.randomBytes(64).toString('hex')
+// const crypto = require('crypto');
+// const JWT_SECRET = crypto.randomBytes(64).toString('hex');
+const JWT_SECRET = 'testSecret';
 
 module.exports = {
     JWT_SECRET,
