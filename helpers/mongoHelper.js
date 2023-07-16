2c026437454f3d58d5c30c688df939e9af918b35
Ngô Văn Trung <Trung2601.it@gmail.com>
2023-07-16 12:13:31 +0700

    [add] increment auto index :sparkles:

---
+++ helpers/mongoHelper.js
@@ -0,0 +1,21 @@
+const {
+    Counter,
+} = require('../models/counterModel');
+
+const getNextSequenceValue = async (sequenceName) => {
+    try {
+        const sequenceDocument = await Counter.findByIdAndUpdate(
+            { _id: sequenceName },
+            { $inc: { seq: 1 } },
+            { new: true, upsert: true }
+        );
+        return sequenceDocument.seq;
+    } catch (err) {
+        console.log(err);
+        return 0;
+    }
+};
+
+module.exports = {
+    getNextSequenceValue
+}
