4b18193ea8e93dfdcaff80926c937c4b1bb0a978
cnvs_minh_vu <nguyenminhvu@canvas-works.asia>
2023-07-20 15:45:15 +0700

    [deploy] env for FRONTEND_URL

--- app.js
+++ app.js
@@ -17,10 +17,7 @@ require('dotenv').config(`${__dirname}/.env.${stage}`)
 
 const corsOptions = {
     origin: [
-        'http://localhost:3000',
-        'http://127.0.0.1:3000',
-        'http://localhost:5173',
-        'http://127.0.0.1:5173',
+        process.env.FRONTEND_URL
     ],
     credentials: true, //access-control-allow-credentials:true
     optionSuccessStatus: 200,
